8ea621109cd804098271fc7186cdd49f8c01e1c1
gumdrop <davidcgood@gmail.com>
2024-12-13 22:23:06 +0000

    Add venues list at small screen sizes

--- client/src/main/scala/quizleague/web/site/venue/VenuesComponent.scala
+++ client/src/main/scala/quizleague/web/site/venue/VenuesComponent.scala
@@ -7,12 +7,19 @@ import quizleague.web.core.GridSizeComponentConfig
 object VenuesComponent extends RouteComponent with GridSizeComponentConfig{
    override val template="""
      <v-container v-bind="gridSize" fluid>
-       <v-layout>
+       <v-layout column>
          <v-flex><ql-text-box><ql-named-text textName="venues-front-page"></ql-named-text></ql-text-box></v-flex>
+         <v-flex v-if="$vuetify.breakpoint.smAndDown">
+         <v-list>
+           <v-list-item :to="'/venue/' + venue.id"  v-for="venue in venues " :key="venue.id">
+             <v-list-item-content><v-list-item-title>{{venue.name}}</v-list-item-title></v-list-item-content>
+           </v-list-item>
+         </v-list>
+         </v-flex>
        </v-layout>
      </v-container>
        """
-
+    subscription("venues")(v => VenueService.list().map(_.sortBy(_.name)))
 }
 object VenuesTitleComponent extends RouteComponent{
    override val  template="""<v-toolbar      
@@ -24,7 +31,7 @@ object VenuesTitleComponent extends RouteComponent{
       <v-toolbar-title>
         Venues
       </v-toolbar-title>
-    </v-toolbar>"""
+   </v-toolbar>"""
        
 
 }
