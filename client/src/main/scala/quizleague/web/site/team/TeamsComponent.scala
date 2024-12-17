d4379b6150663b3307d3b45c4c87050b87e764c8
gumdrop <davidcgood@gmail.com>
2024-12-17 21:11:31 +0000

    Teams in body at small sizes

--- client/src/main/scala/quizleague/web/site/team/TeamsComponent.scala
+++ client/src/main/scala/quizleague/web/site/team/TeamsComponent.scala
@@ -9,8 +9,13 @@ import quizleague.web.site.login.LoginService
 object TeamsComponent extends RouteComponent with GridSizeComponentConfig {
     val template="""
       <v-container v-bind="gridSize" fluid>
-        <v-layout>
+        <v-layout column>
           <v-flex><ql-text-box><ql-named-text textName="teams-header"></ql-named-text></ql-text-box></v-flex>
+          <v-flex v-if="$vuetify.breakpoint.smAndDown">
+            <v-list-item v-if="teams" v-for="team in teams" :key="team.id" :to="'/team/' + team.id">
+              <v-list-item-content><v-list-item-title v-text="team.name"></v-list-item-title></v-list-item-content>
+           </v-list-item>
+          </v-flex>
         </v-layout>
        </v-container>"""
     override val mounted = ({(c:facade) => {
@@ -18,6 +23,8 @@ object TeamsComponent extends RouteComponent with GridSizeComponentConfig {
     LoginService.userProfile.filter(_ != null).subscribe(u => c.$router.push(s"/team/${u.team.id}"))
 
   }}:js.ThisFunction)
+
+    subscription("teams")(c => TeamService.list().map(_.sortBy(_.shortName)))
 
 }
 object TeamsTitleComponent extends RouteComponent{
